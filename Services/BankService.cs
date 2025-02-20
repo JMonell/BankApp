0e88da9d32fa255c2945eaba81d150be0fb49a56
JMonell <joar.monell@gmail.com>
2025-02-20 01:19:09 +0100

    adding delete button and function

--- Services/BankService.cs
+++ Services/BankService.cs
@@ -1,6 +1,7 @@
 
 using BankApp.Data;
 using BankApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -10,6 +11,7 @@ public class BankService{
     private readonly ApplicationDbContext _context;
     private Account account;
     private List<Account> accounts = new List<Account>();
+    private ApplicationUser user;
     
 
 
@@ -24,31 +26,83 @@ public class BankService{
         await _context.SaveChangesAsync();
     }
 
-    public async Task<List<Account>> GetAccountsByUser(ApplicationUser user)
+    public async Task<List<Account>> GetAccountsByUserAsync(ApplicationUser user)
     {
         return await _context.Accounts.Where(a => a.User.Id == user.Id).ToListAsync();
     }
 
     // Method to create and save 'Main Account' for each new user
-     public async Task CreateMainAccountAsync(ApplicationUser user)
-     {
+    public async Task CreateMainAccountAsync(ApplicationUser user){
           // Default properties (for Register.razor as well)
-          var mainAccount = new Account
-          {
-               AccountNumber = await GetAccountnumber(), // Create a method to generate an Account Number!
-               Name = "Main Account",
-               Balance = 100000.00m,
-               AccountType = AccountTypes.Main, // Bringing this from the enum in AccountType (Account.cs)
-               IsActive = true,
-               UserId = user.Id,
-               CardNumber = "1234 5678 9010 1112"
-          };
-          // Adding the new account to the database
-          _context.Accounts.Add(mainAccount);
-          // Saving changes asynchronously in the database
-          await _context.SaveChangesAsync();
-     }
+        if(user.Accounts.Any() == false){
+        var mainAccount = new Account
+        {
+            AccountNumber = await GetAccountnumber(), // Create a method to generate an Account Number!
+            Name = "Main Account",
+            Balance = 100000.00m,
+            AccountType = AccountTypes.Main, // Bringing this from the enum in AccountType (Account.cs)
+            IsActive = true,
+            UserId = user.Id,
+            CardNumber = "1234 5678 9010 1112"
+        };
+        // Adding the new account to the database
+        _context.Accounts.Add(mainAccount);
+        // Saving changes asynchronously in the database
+        await _context.SaveChangesAsync();
+        }
+    }
 
+    // public async Task DeleteAccountAsync(int accountId){
+    //     var account = await _context.Accounts.FindAsync(accountId);
+    //     if (account != null){
+    //         await _context.Accounts.DeleteAsync(account);
+            
+    //     }
+    // }
+
+    // public async Task DeleteAccountAsync(int accountId){
+    //     await _context.Accounts
+    //           //FirstOrDefaultAsync checks the first matching account and if it doesn't exist return default(NULL)
+    //         .FirstOrDefaultAsync(a => a.Id == account.Id && a.UserId == user.Id);
+
+    //     if (account != null){
+    //         _context.Accounts.Remove(account);
+    //         await _context.SaveChangesAsync();
+    //     }
+    // }
+    
+/*
+
+private async Task DeleteUser(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user != null){
+            var roles = await userManager.GetRolesAsync(user); // Get user roles
+            if (roles.Contains("User")){
+                await userManager.DeleteAsync(user);
+                await ShowUsers(); // Refresh user list
+            }
+            else{
+                Console.WriteLine("can not delete Admin");
+                await ShowUsers(); // Refresh user list
+            }
+        }
+
+        
+    }
+
+*/
+        public async Task DeleteAccountAsync(ApplicationUser user, int accountId)
+        {
+            var account = await _context.Accounts
+                .FirstOrDefaultAsync(a => a.Id == accountId && a.UserId == user.Id);
+
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+                await _context.SaveChangesAsync();
+            }
+        }
 
     // private async Task SaveAccount(ApplicationUser user)
     // {
